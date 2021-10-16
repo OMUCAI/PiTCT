ded92529cc85377775907967534c6aae929daa90
Masahiro Kinishi <konikoni428@gmail.com>
2021-10-16 15:01:48 +0900

    remove update sdist

--- build.py
+++ build.py
@@ -1,10 +1,6 @@
 from setuptools import Extension
 from pathlib import Path
-import tarfile
-from pathlib import Path
-import os
-import glob
-import shutil
+
 
 build_file_c = [str(cfile) for cfile in Path("pytct/").glob("**/*.c")]
 build_file_cpp = [str(cppfile) for cppfile in Path("pytct/").glob("**/*.cpp")]
@@ -18,33 +14,8 @@ extensions = [
     )
 ]
 
-def update_sdist():
-    # search latest sdist file
-    files = glob.glob("dist/*.tar.gz")
-    files.sort(key=os.path.getmtime, reverse=True)
-    if len(files) < 1:
-        return
-    filename = files[0]
-
-    # extract content
-    with tarfile.open(filename, mode='r:gz') as tar:
-        tar.extractall("dist/sdist")
-    
-    # copy source
-    dir = glob.glob("dist/sdist/PyTCT-*")[0]
-    shutil.copytree("pytct/libtct", f"{dir}/pytct/libtct")
-
-    # compress sdist
-    with tarfile.open(filename, "w:gz") as tar:
-        tar.add("dist/sdist", "/")
-    
-    # remove tmp content file
-    shutil.rmtree("dist/sdist")
-
 def build(setup_kwargs):
     """
     This function is mandatory in order to build the extensions.
     """
-    print("Add C Source to sdist")
-    update_sdist()
     setup_kwargs.update({"ext_modules": extensions})
