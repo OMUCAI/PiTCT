4c30f607a8512c574d694ccf699ad2bd8d7fc068
Masahiro Konishi <konikoni428@gmail.com>
2022-07-17 05:20:28 +0900

    fix convert problem #6, #7

--- pytct/name_converter.py
+++ pytct/name_converter.py
@@ -8,23 +8,34 @@ def get_key_from_value(d, val):
 
 
 class NameConverter:
-    # event_encode_dict = {
+    # state_encode_dict = {
     #     'SAMPLE': {
-    #         0: 'run',
-    #         1: 'broken'
+    #         0: 'aaaa',
+    #         1: 'bbbb'
     #     }
     # }
+
+    # event_encode_dict = {
+    #     1: 'go',
+    #     3: 'back'
+    # }
+    
     event_encode_dict = {}
     state_encode_dict = {}
     event_already_use = -1
     event_uncont_already_use = -2
 
     @classmethod
-    def encode_all(cls, name: str, trans_list: TransList):
-        if name in cls.state_encode_dict.keys():
-            # reset already exist
-            cls.state_encode_dict[name] = {}
+    def reset(cls):
+        cls.event_encode_dict = {}
+        cls.state_encode_dict = {}
+        cls.event_already_use = -1
+        cls.event_uncont_already_use = -2
 
+    @classmethod
+    def encode_all(cls, name: str, trans_list: TransList) -> TransList:
+        cls.state_encode_dict[name] = {}
+        
         encoded = []
         for s, e, ns, *uc in trans_list:
             state_num = cls.state_encode(name, s)
@@ -51,12 +62,13 @@ class NameConverter:
     @classmethod
     def event_encode(cls, name: str, event: Event, is_uncontrollable = False) -> int:
         if isinstance(event, str):
-            if not name in cls.event_encode_dict.keys():
-                cls.event_encode_dict[name] = {}
-
-            if event in cls.event_encode_dict[name].values():
+            if event in cls.event_encode_dict.values():
                 # alredy register
-                return get_key_from_value(cls.event_encode_dict[name], event)
+                event_num = get_key_from_value(cls.event_encode_dict, event)
+                calc_uncont = event_num % 2 == 0
+                if is_uncontrollable != calc_uncont:
+                    raise RuntimeError(f"Detect same name controllable and uncontrollable event (event: {event}). If you change c or u, please run pytct.init().")
+                return event_num
 
             # calculate event number
             if is_uncontrollable:
@@ -67,7 +79,7 @@ class NameConverter:
                 cls.event_already_use = attach_num
             
             # register event num : event str mapping
-            cls.event_encode_dict[name][attach_num] = event
+            cls.event_encode_dict[attach_num] = event
             return attach_num
         else:
             return event
@@ -96,12 +108,8 @@ class NameConverter:
         if not convert:
             return str(event)
         
-        if not name in cls.event_encode_dict.keys():
-            return str(event)
-        
-        conv = cls.event_encode_dict[name]
         try:
-            return conv[event]
+            return cls.event_encode_dict[event]
         except KeyError:
             return str(event)
 
