e73e31acd3741c65e631b21b76a94ba2602fd59f
Masahiro Konishi <konikoni428@gmail.com>
2022-11-12 03:28:27 +0900

    Update typing

--- pytct/typing.py
+++ pytct/typing.py
@@ -1,11 +1,16 @@
 from __future__ import annotations
-from typing import Union
-
+from typing import Union, Tuple, List  # List, Tupleは3.9から非推奨
+try:
+    from typing import Literal
+except ImportError:
+    # python3.7
+    from typing_extensions import Literal
 
 Event = Union[int, str]
 State = Union[int, str]
-Trans1 = tuple[State, Event, State]
-Trans2 = tuple[State, Event, State, bool]
+CorUC = Literal['c', 'u']  # Controllable or Uncontrollable
+Trans1 = Tuple[State, Event, State]
+Trans2 = Tuple[State, Event, State, CorUC]
 Trans = Union[Trans1, Trans2]
-TransList = list[Trans]
-StateList = list[State]
+TransList = List[Trans]
+StateList = List[State]
