247934ee0f58a96cb6d42141923815ea7ec1f2e1
Masahiro Konishi <konikoni428@gmail.com>
2023-02-07 04:52:57 +0900

    delete typing List, Tuple

--- pytct/tct_typing.py
+++ pytct/tct_typing.py
@@ -1,11 +1,11 @@
 from __future__ import annotations
-from typing import Union, Tuple, List, Literal  # List, Tupleは3.9から非推奨
+from typing import Union, Literal  # List, Tupleは3.9から非推奨
 
 Event = Union[int, str]
 State = Union[int, str]
 CorUC = Literal['c', 'u']  # Controllable or Uncontrollable
-Trans1 = Tuple[State, Event, State]
-Trans2 = Tuple[State, Event, State, CorUC]
+Trans1 = tuple[State, Event, State]
+Trans2 = tuple[State, Event, State, CorUC]
 Trans = Union[Trans1, Trans2]
-TransList = List[Trans]
-StateList = List[State]
+TransList = list[Trans]
+StateList = list[State]
