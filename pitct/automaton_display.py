f158c0545587189fd157261b03cd4c5dfd9cfb11
Masahiro Konishi <konikoni428@gmail.com>
2026-01-05 23:43:04 +0900

    Add support for pygraphviz in AutomatonDisplay class

--- pitct/automaton_display.py
+++ pitct/automaton_display.py
@@ -1,5 +1,10 @@
 import umsgpack
 import graphviz as gv
+try:
+    import pygraphviz as pgv
+    PYGRAPHVIZ_AVAILABLE = True
+except ImportError:
+    PYGRAPHVIZ_AVAILABLE = False
 from pathlib import Path
 from datetime import datetime
 import tempfile
@@ -21,27 +26,47 @@ class AutomatonDisplay(object):
 
         states = self.__data["states"]
 
-        self.__graph = gv.Digraph("finite_state_machine", strict=False)
-        self.__graph.attr(rankdir="LR")
+        self.__use_pygraphviz = PYGRAPHVIZ_AVAILABLE
+        if self.__use_pygraphviz:
+            self.__graph = pgv.AGraph(directed=True, strict=False)
+            self.__graph.graph_attr['rankdir'] = 'LR'
+        else:
+            self.__graph = gv.Digraph("finite_state_machine", strict=False)
+            self.__graph.attr(rankdir="LR")
 
         if self.__data["size"] < 1:
-            self.__graph.node("[empty]", color="white")
+            if self.__use_pygraphviz:
+                self.__graph.add_node("[empty]", color="white")
+            else:
+                self.__graph.node("[empty]", color="white")
             return
 
         # add the initial entry state
-        self.__graph.node("_a", shape="point", color="white")
+        if self.__use_pygraphviz:
+            self.__graph.add_node("_a", shape="point", color="white")
+        else:
+            self.__graph.node("_a", shape="point", color="white")
 
         # add states
         for label, state in states.items():
             conv_label = str(NameConverter.state_decode(plant, label, convert))
-            if state["marked"]:
-                self.__graph.node(conv_label, shape="doublecircle")
+            if self.__use_pygraphviz:
+                if state["marked"]:
+                    self.__graph.add_node(conv_label, shape="doublecircle")
+                else:
+                    self.__graph.add_node(conv_label, shape="circle")
             else:
-                self.__graph.node(conv_label, shape="circle")
+                if state["marked"]:
+                    self.__graph.node(conv_label, shape="doublecircle")
+                else:
+                    self.__graph.node(conv_label, shape="circle")
 
         # add the initial entry edge
         initial_state = str(NameConverter.state_decode(plant, 0, convert))
-        self.__graph.edge("_a", initial_state)
+        if self.__use_pygraphviz:
+            self.__graph.add_edge("_a", initial_state)
+        else:
+            self.__graph.edge("_a", initial_state)
 
         # add transitions
         for label in states:
@@ -49,19 +74,26 @@ class AutomatonDisplay(object):
             if trans is not None:
                 for tran in trans:
                     label_text = str(NameConverter.event_decode(tran[0], convert))
-                    if color:
-                        self.__graph.edge(
-                            str(NameConverter.state_decode(plant, label, convert)),
-                            str(NameConverter.state_decode(plant, tran[1], convert)),
-                            label=label_text,
-                            color="red" if tran[0] % 2 == 1 else "green",
-                        )
+                    src = str(NameConverter.state_decode(plant, label, convert))
+                    dst = str(NameConverter.state_decode(plant, tran[1], convert))
+                    if self.__use_pygraphviz:
+                        if color:
+                            self.__graph.add_edge(
+                                src, dst,
+                                label=label_text,
+                                color="red" if tran[0] % 2 == 1 else "green",
+                            )
+                        else:
+                            self.__graph.add_edge(src, dst, label=label_text)
                     else:
-                        self.__graph.edge(
-                            str(NameConverter.state_decode(plant, label, convert)),
-                            str(NameConverter.state_decode(plant, tran[1], convert)),
-                            label=label_text
-                        )
+                        if color:
+                            self.__graph.edge(
+                                src, dst,
+                                label=label_text,
+                                color="red" if tran[0] % 2 == 1 else "green",
+                            )
+                        else:
+                            self.__graph.edge(src, dst, label=label_text)
 
     def set_attr(self,
         layout="dot",
@@ -72,20 +104,34 @@ class AutomatonDisplay(object):
     ):
         new_label = self.__path.name if label is None else str(label)
 
-        if timelabel:
-            self.__graph.attr(
-                "graph",
-                label="{}\n{}".format(new_label, datetime.now().isoformat(sep=" ")),
-            )
-        else:
-            self.__graph.attr("graph", label=new_label)
+        if self.__use_pygraphviz:
+            if timelabel:
+                self.__graph.graph_attr['label'] = "{}\n{}".format(new_label, datetime.now().isoformat(sep=" "))
+            else:
+                self.__graph.graph_attr['label'] = new_label
 
-        if dpi:
-            self.__graph.attr("graph", dpi=str(dpi))
-        
-        self.__graph.attr("graph", layout=layout)
-        if len(kwargs) > 0:
-            self.__graph.attr("graph", **kwargs)
+            if dpi:
+                self.__graph.graph_attr['dpi'] = str(dpi)
+            
+            self.__graph.graph_attr['layout'] = layout
+            if len(kwargs) > 0:
+                for k, v in kwargs.items():
+                    self.__graph.graph_attr[k] = v
+        else:
+            if timelabel:
+                self.__graph.attr(
+                    "graph",
+                    label="{}\n{}".format(new_label, datetime.now().isoformat(sep=" ")),
+                )
+            else:
+                self.__graph.attr("graph", label=new_label)
+
+            if dpi:
+                self.__graph.attr("graph", dpi=str(dpi))
+            
+            self.__graph.attr("graph", layout=layout)
+            if len(kwargs) > 0:
+                self.__graph.attr("graph", **kwargs)
 
     def save(
         self,
@@ -100,7 +146,11 @@ class AutomatonDisplay(object):
         self.set_attr(layout=layout, dpi=dpi, label=label, timelabel=timelabel, **kwargs)
 
         path_filename = Path(conf.SAVE_FOLDER / filename)
-        self.__graph.render(path_filename, format=fileformat, cleanup=True)
+        if self.__use_pygraphviz:
+            self.__graph.layout(prog=layout)
+            self.__graph.draw(str(path_filename) + '.' + fileformat, format=fileformat)
+        else:
+            self.__graph.render(path_filename, format=fileformat, cleanup=True)
 
     def render(self,
         layout="dot",
@@ -116,15 +166,30 @@ class AutomatonDisplay(object):
             return self
         else:
             # shell
-            with tempfile.NamedTemporaryFile(delete=False, suffix='.gv') as tmp:
-                tmp_path = tmp.name
-            return self.__graph.render(tmp_path, view=True, format=format, cleanup=True)
+            if self.__use_pygraphviz:
+                temp_file = tempfile.mktemp('.gv')
+                self.__graph.layout(prog=layout)
+                self.__graph.draw(temp_file + '.' + format, format=format)
+                # pygraphvizはビューワーを自動で開かないので、graphvizの動作を真似る
+                import subprocess
+                subprocess.run(['xdg-open', temp_file + '.' + format])
+                return temp_file + '.' + format
+            else:
+                return self.__graph.render(tempfile.mktemp('.gv'), view=True, format=format, cleanup=True)
 
     def _repr_html_(self):
-        svg = self.__graph._repr_svg_()
+        if self.__use_pygraphviz:
+            self.__graph.layout(prog='dot')
+            svg = self.__graph.draw(format='svg').decode('utf-8')
+        else:
+            svg = self.__graph._repr_svg_()
         # svg文字列をb64エンコードしてから埋め込み
         html = BASE_HTML.format("100%", base64.b64encode(svg.encode()).decode())
         return html
 
     def _repr_svg_(self):
-        return self.__graph._repr_svg_()
+        if self.__use_pygraphviz:
+            self.__graph.layout(prog='dot')
+            return self.__graph.draw(format='svg').decode('utf-8')
+        else:
+            return self.__graph._repr_svg_()
