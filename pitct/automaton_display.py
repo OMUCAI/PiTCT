ebca60f3842c75df3c8262b0b6f73b8a7c803608
Masahiro Konishi <konikoni428@gmail.com>
2026-01-07 04:28:20 +0900

    Add JupyterLite support for rendering graphs in AutomatonDisplay

--- pitct/automaton_display.py
+++ pitct/automaton_display.py
@@ -8,15 +8,10 @@ from pitct.name_converter import NameConverter
 from .util import is_env_notebook, is_env_jupyterlite
 from .config import Config, DES_FILE_EXTENSION
 import base64
-
-# For JupyterLite
-try:
-    from jupyter_anywidget_graphviz import graphviz_headless
-    HAS_ANYWIDGET = True
-except ImportError:
-    HAS_ANYWIDGET = False
+import uuid
 
 BASE_HTML = '<img width="{}" src="data:image/svg+xml;base64,{}" >'
+HPCC_WASM_URL = "https://cdn.jsdelivr.net/npm/@hpcc-js/wasm-graphviz@1.18.0/+esm"
 
 conf = Config.get_instance()
 
@@ -131,16 +126,49 @@ class AutomatonDisplay(object):
             return self.__graph.render(tmp_path, view=True, format=format, cleanup=True)
 
     def _repr_html_(self):
-        svg = self.__graph._repr_svg_()
-        # svg文字列をb64エンコードしてから埋め込み
-        html = BASE_HTML.format("100%", base64.b64encode(svg.encode()).decode())
-        return html
+        if is_env_jupyterlite():
+            dot = self.__graph.source
+            return self._jupyterlite_html_(dot)
+        else:
+            svg = self.__graph._repr_svg_()
+            # svg文字列をb64エンコードしてから埋め込み
+            html = BASE_HTML.format("100%", base64.b64encode(svg.encode()).decode())
+            return html
 
     def _repr_svg_(self):
-        if is_env_jupyterlite() and HAS_ANYWIDGET:
-            g = graphviz_headless()
-            g.set_code_content(self.__graph.source)
-            g.blocking_reply()
-            return g.svg
-        else:
-            return self.__graph._repr_svg_()
+        return self.__graph._repr_svg_()
+
+    def _jupyterlite_html_(self, dot: str):
+        # generate uuid
+        uuidstr = "graphviz-" + str(uuid.uuid4())
+        return f"""
+<div id="{uuidstr}"></div>
+<script type="module">
+    (async () => {{
+        const div = document.getElementById("{uuidstr}");
+        const dot = `{dot}`;
+        
+        try {{
+            // A. Search for pre-loaded graphviz
+            let loadPromise = window.jupyterGraphvizPromise;
+            
+            // B. Fallback to dynamic import if not found
+            if (!loadPromise) {{
+                console.warn("Graphviz not pre-loaded. Loading now...");
+                const {{ Graphviz }} = await import("{self.HPCC_WASM_URL}");
+                // save the promise to avoid re-loading
+                window.jupyterGraphvizPromise = Graphviz.load();
+                loadPromise = window.jupyterGraphvizPromise;
+            }}
+            
+            // C. write the graph
+            const graphviz = await loadPromise;
+            const svg = graphviz.dot(dot);
+            div.innerHTML = svg;  
+        }} catch (err) {{
+            div.innerHTML = `<div style="color:red">Error: ${{err.message}}</div>`;
+            console.error(err);
+        }}
+    }})();
+</script>
+"""
